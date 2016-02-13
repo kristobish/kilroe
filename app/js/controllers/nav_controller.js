3a2e86f97f2c891c787ecf6f05ff2b55491561f9
kristobish <kristobish@gmail.com>
2016-02-13 14:13:12 -0500

    Temp. fix for mobile active nav states

--- app/js/controllers/nav_controller.js
+++ app/js/controllers/nav_controller.js
@@ -3,9 +3,16 @@
 
     app.controller('NavCtrl', function ($scope, $location) {
 
-        $scope.isActive = function(route) {
+        $scope.isActive = function (route) {
             return route === $location.path();
-        }
+        };
+
+        $(function() {
+            $(".pushy a").click(function() {
+                $(".pushy a").removeClass("active");
+                $(this).addClass("active");
+            });
+        });
 
     });
 })(angular.module('kilroe'));
