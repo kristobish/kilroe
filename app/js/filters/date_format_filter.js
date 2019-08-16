2ae96ac18a9b1fc193357927e0f277a6621730f6
kristobish <kristobish@gmail.com>
2019-08-15 21:34:03 -0400

    added filters when adding new gigs

--- app/js/filters/date_format_filter.js
+++ app/js/filters/date_format_filter.js
@@ -1,5 +1,6 @@
 (function (app) {
     'use strict';
+    /*global moment */
 
     app.filter('dateFormatGig', function ($filter) {
         return function (input) {
@@ -18,6 +19,22 @@
             }
 
             return $filter('date')(new Date(input), 'M.d.yy');
+        };
+    });
+
+    app.filter('isFuture', function() {
+        return function(gigs) {
+            return gigs.filter(function(gig){
+                return moment(gig.date, 'YYYY-MM-DD').isSameOrAfter(moment().format('YYYY-MM-DD'));
+            });
+        };
+    });
+
+    app.filter('isPast', function() {
+        return function(gigs) {
+            return gigs.filter(function(gig){
+                return moment(gig.date, 'YYYY-MM-DD').isBefore(moment().format('YYYY-MM-DD'));
+            });
         };
     });
 
